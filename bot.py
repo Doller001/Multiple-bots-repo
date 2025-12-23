2e2534df282413c47e589fd5c00c5dc2b46be1d2
Minaty <saif.ali321554@gmail.com>
2025-12-23 17:54:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,16 @@
-from flask import Flask
-import threading
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-from handlers import start, callbacks
-from admin import admin_handlers
+from handlers import start, callbacks, stats, broadcast
 
-BOT_TOKEN = "8236683990:AAGuQN5QKhcx7peERy9yUe_sR0qrS7RvEYA"
-
-# 🌐 Flask (Render + UptimeRobot)
-app = Flask(__name__)
-@app.route("/")
-def home():
-    return "Bot is running"
-
-def run_flask():
-    app.run(host="0.0.0.0", port=10000)
+BOT_TOKEN = "PASTE_YOUR_TOKEN"
 
 def main():
-    threading.Thread(target=run_flask).start()
-
     updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("stats", stats))
+    dp.add_handler(CommandHandler("broadcast", broadcast))
     dp.add_handler(CallbackQueryHandler(callbacks))
-    admin_handlers(dp)
 
     updater.start_polling()
     updater.idle()
